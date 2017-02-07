12a2a7f3c93c664cdc5a4f159026c8222dc115b3
Philip177 <PhilJCoh@Gmail.com>
2017-02-07 17:54:45 -0500

    Add files via upload

---
+++ Week 1/sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+createCanvas(640,480);
+line(55,25,70,800);
+background('green');
+fill('blue')
+//    two points, numbers in (x1, y1, x2, y2)
+    //head
+    ellipse(300,300,600);
+    //eyes
+fill('#f4d85d')
+arc(150, 50, 50, 75, 0, PI + 1);
+
+    ellipse(150, 230, 150, 75);
+     ellipse(450, 230, 150, 75);
+    //nose
+    triangle(30, 75, 58, 20, 86, 75);
+    line(90,515,300,515);
+}
