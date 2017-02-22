89dd1da848d0954cdb9446c5e8c287cdf1c26117
Philip177 <PhilJCoh@Gmail.com>
2017-02-22 15:45:36 -0600

    Week 3 complete

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -8,6 +8,31 @@ createCanvas(640, 480);
     var ts= 60;
     var linenumber = 1;
     
+    var skinbase = ("yellow");
+    fill(skinbase);
+    ellipse (320, 250, 320, 250);
+    
+     var skinbase = ("yellow");
+    fill(skinbase);
+    ellipse (113, 421, 150, 167);
+    
+     var skinbase = ("yellow");
+    fill(skinbase);
+    ellipse (558, 120, 50, 43);
+    
+    fill(skinbase);
+    ellipse (601, 267, 32, 124);
+    
+    fill(skinbase);
+    ellipse (22, 89, 395, 90);
+    
+    var lavaLampColor = ("purple");
+    noFill();
+    stroke(lavaLampColor);
+    strokeWeight(30);
+    ellipse (320, 240, 640, 1500);
+    fill("white");
+    
     textSize(ts);
     textFont("Moncaco");
     textAlign(CENTER);
@@ -23,11 +48,16 @@ createCanvas(640, 480);
     rectMode(CENTER);
     var intro = "Where all your dreams";
     var continued = "burn with passion.";
+    var ending =  "© symbol"
        
     rect(x, y +ts/2, greetingWidth, ts);
+    fill("black")
     text(greeting, x, y + ts*linenumber);
     linenumber = linenumber +1;
     text(intro, x, y + ts*linenumber);
     linenumber = linenumber +1;
     text(continued, x, y + ts*linenumber);
+    linenumber = linenumber +1;
+    text(ending, x, y + ts*linenumber + 200); 
+    
 }
